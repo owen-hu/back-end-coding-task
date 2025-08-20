26109bc8ac47e9995928d91052dd1d219aa70091
Owen Huggard <69146888+Eoghan-Inky@users.noreply.github.com>
2025-08-20 03:38:17 +0200

    Comments

--- Claims/ApiLayer/ClaimDto.cs
+++ Claims/ApiLayer/ClaimDto.cs
@@ -4,11 +4,33 @@ namespace Claims.ApiLayer;
 
 public class ClaimDto
 {
+    /// <summary>
+    /// ID: Ignored when adding
+    /// </summary>
     public string? Id { get; set; }
+    
+    /// <summary>
+    /// Must have a corresponding cover
+    /// </summary>
     public string CoverId { get; set; }
+    
+    /// <summary>
+    /// YYYY-MM-DD
+    /// </summary>
     public DateOnly CreatedDate { get; set; }
+    
+    /// <summary>
+    /// Name of Claim
+    /// </summary>
     public string Name { get; set; }
     
+    /// <summary>
+    /// e.g Fire, Collision etc.
+    /// </summary>
     public ClaimType ClaimType { get; set; }
+    
+    /// <summary>
+    /// Amount in USD? NOK? Who knows...
+    /// </summary>
     public decimal DamageCost { get; set; }
 }
